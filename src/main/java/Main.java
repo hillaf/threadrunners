82c4107957396d0d535601eeeab511cbe6db7c1d
Hilla Fred <hilla.fred@helsinki.fi>
2017-02-07 13:51:11 +0200

    Java template for thread exercises

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String [ ] args) {
+
+
+        (new Runner("Ann")).start();
+        (new Runner("Bill")).start();
+        (new Runner("Charlie")).start();
+
+    }
+}
+
---
+++ src/main/java/Runner.java
@@ -0,0 +1,53 @@
+import java.lang.*;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+
+public class Runner extends Thread
+{
+    // let's use semaphore with an access count of 1 as our mutex
+    static Semaphore semaphore = new Semaphore(1);
+    int counter = 0;
+    String name;
+
+    public Runner(String name){
+        this.name = name;
+    }
+
+    public void run()
+    {
+        for (int i = 0; i<10; i++){
+
+            if (this.name.equalsIgnoreCase("Ann")){
+                // do something here, eg. semaphore.acquire();
+            }
+
+            if (this.name.equalsIgnoreCase("Bill")){
+                // do something here
+            }
+
+            if (this.name.equalsIgnoreCase("Charlie")){
+                // do something here
+            }
+
+            // let's run a lap!
+            this.counter++;
+            System.out.println(this.name + " " + this.counter);
+
+            try {
+                // let's slow everyone down a bit
+                Thread.sleep(500);
+
+                // let's make bill a bit slower than anyone else to see the results
+                if (this.name.equalsIgnoreCase("Bill")){
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        // runner finished!
+        System.out.println(this.name + " finished!");
+    }
+}
